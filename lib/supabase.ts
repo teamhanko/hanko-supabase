c6e5082b6851bf7b24d35d1627a3a26d88bea231
Florian Schneider <51193406+firelocklp@users.noreply.github.com>
2023-12-19 14:22:56 +0100

    Update supabase.ts
    Updated the Environment Variable according to the Docs PR: 
    https://github.com/teamhanko/docs/commit/0b41d02a88163c515739693913b0e5b5db85adb4

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export { supabase };
